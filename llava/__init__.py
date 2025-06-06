38ec6a97598f5da7934a4877adea08348ebf5526
Madhav Lolla <siva.lolla@gmail.com>
2025-06-06 17:04:04 -0400

    removed .whl

--- llava/__init__.py
+++ llava/__init__.py
@@ -1,1 +1,5 @@
-from .model import LlavaLlamaForCausalLM
+#from .model import LlavaLlamaForCausalLM
+#import llava.model 
+from .model.language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
+#from .model.language_model.llava_mpt import LlavaMptForCausalLM, LlavaMptConfig
+#from .model.language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
